594ef4d56a7f15c91643950fc2756c3e2c114231
Milton Pividori <miltondp@gmail.com>
2017-03-17 16:10:25 -0500

    Support to arguments link host and port.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from os.path import isdir
 from flask import Flask
 from flask_restful import Api
 
@@ -11,17 +12,23 @@ api = Api(app)
 
 api.add_resource(
     ChromosomeAPI,
-    '/ukbrest/api/v1.0/chromosomes/<int:chr>/positions/<int:start>/<int:stop>'
+    '/ukbrest/api/v1.0/chromosomes/<int:chr>/variants/<int:start>/<int:stop>'
 )
 
 if __name__ == '__main__':
+    from ipaddress import ip_address
     import argparse
 
     parser = argparse.ArgumentParser()
     parser.add_argument('repository_path', type=str, help='UK Biobank repository path')
+    parser.add_argument('--host', dest='host', type=ip_address, required=False, help='Host', default=ip_address('127.0.0.1'))
+    parser.add_argument('--port', dest='port', type=int, required=False, help='Port', default=5000)
 
     args = parser.parse_args()
 
+    if not isdir(args.repository_path):
+        raise Exception('Repository path does not exist: {}'.format(args.repository_path))
+
     app.config.update({'ukbquery': UKBQuery(args.repository_path)})
 
-    app.run(debug=True)
+    app.run(host=str(args.host), port=args.port, debug=True)
