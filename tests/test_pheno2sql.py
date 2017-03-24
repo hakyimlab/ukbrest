35006cc66412fb1b025c28b7f8e05c37030c4928
Milton Pividori <miltondp@gmail.com>
2017-03-24 16:24:12 -0500

    pheno2sql: more unit tests.

--- tests/test_pheno2sql.py
+++ tests/test_pheno2sql.py
@@ -67,8 +67,8 @@ class Pheno2SQLTest(unittest.TestCase):
         assert p2sql.db_type == 'postgresql'
 
         ## Check table exists
-        tmp = pd.read_sql("SELECT EXISTS (SELECT 1 FROM pg_tables WHERE schemaname = 'public' AND tablename = '{}');".format('ukb_pheno_00'), create_engine(db_engine))
-        assert not tmp.empty
+        table = pd.read_sql("SELECT EXISTS (SELECT 1 FROM pg_tables WHERE schemaname = 'public' AND tablename = '{}');".format('ukb_pheno_00'), create_engine(db_engine))
+        assert table.iloc[0, 0]
 
         ## Check columns are correct
         tmp = pd.read_sql('select * from ukb_pheno_00', create_engine(db_engine))
@@ -108,8 +108,8 @@ class Pheno2SQLTest(unittest.TestCase):
 
         # Validate
         ## Check table exists
-        tmp = pd.read_sql("SELECT EXISTS (SELECT 1 FROM pg_tables WHERE schemaname = 'public' AND tablename = '{}');".format('ukb_pheno_00'), create_engine(db_engine))
-        assert not tmp.empty
+        table = pd.read_sql("SELECT EXISTS (SELECT 1 FROM pg_tables WHERE schemaname = 'public' AND tablename = '{}');".format('ukb_pheno_00'), create_engine(db_engine))
+        assert table.iloc[0, 0]
 
         ## Check columns are correct
         tmp = pd.read_sql('select * from ukb_pheno_00', create_engine(db_engine))
@@ -124,3 +124,139 @@ class Pheno2SQLTest(unittest.TestCase):
 
         ## Check that temporary files were deleted
         assert len(os.listdir('/tmp/ukbrest')) == 0
+
+    def test_sqlite_less_columns_per_table(self):
+        # Prepare
+        csv_file = get_repository_path('pheno2sql/example01.csv')
+        db_engine = 'sqlite:///tmp.db'
+
+        p2sql = Pheno2SQL(csv_file, db_engine, n_columns_per_table=3)
+
+        # Run
+        p2sql.load_data()
+
+        # Validate
+        assert p2sql.db_type == 'sqlite'
+
+        ## Check tables exist
+        table = pd.read_sql("SELECT name FROM sqlite_master WHERE type='table' AND name='{}';".format('ukb_pheno_00'), create_engine(db_engine))
+        assert not table.empty
+
+        table = pd.read_sql("SELECT name FROM sqlite_master WHERE type='table' AND name='{}';".format('ukb_pheno_01'), create_engine(db_engine))
+        assert not table.empty
+
+        table = pd.read_sql("SELECT name FROM sqlite_master WHERE type='table' AND name='{}';".format('ukb_pheno_02'), create_engine(db_engine))
+        assert not table.empty
+
+        ## Check columns are correct
+        tmp = pd.read_sql('select * from ukb_pheno_00', create_engine(db_engine))
+        expected_columns = ["eid","21-0.0","21-1.0","21-2.0"]
+        assert len(tmp.columns) == len(expected_columns)
+        assert all(x in expected_columns for x in tmp.columns)
+
+        tmp = pd.read_sql('select * from ukb_pheno_01', create_engine(db_engine))
+        expected_columns = ["eid","31-0.0","34-0.0","46-0.0"]
+        assert len(tmp.columns) == len(expected_columns)
+        assert all(x in expected_columns for x in tmp.columns)
+
+        tmp = pd.read_sql('select * from ukb_pheno_02', create_engine(db_engine))
+        expected_columns = ["eid","47-0.0","48-0.0"]
+        assert len(tmp.columns) == len(expected_columns)
+        assert all(x in expected_columns for x in tmp.columns)
+
+        ## Check data is correct
+        tmp = pd.read_sql('select * from ukb_pheno_00', create_engine(db_engine), index_col='eid')
+        assert not tmp.empty
+        assert tmp.shape[0] == 2
+        assert tmp.loc[1, '21-0.0'] == 'Option number 1'
+        assert tmp.loc[1, '21-1.0'] == 'No response'
+        assert tmp.loc[1, '21-2.0'] == 'Yes'
+        assert tmp.loc[2, '21-0.0'] == 'Option number 2'
+        assert tmp.loc[2, '21-1.0'] == ''
+        assert tmp.loc[2, '21-2.0'] == 'No'
+
+        tmp = pd.read_sql('select * from ukb_pheno_01', create_engine(db_engine), index_col='eid')
+        assert not tmp.empty
+        assert tmp.shape[0] == 2
+        assert tmp.loc[1, '31-0.0'] == '2012-01-05'
+        assert tmp.loc[1, '34-0.0'] == 21
+        assert tmp.loc[1, '46-0.0'] == -9
+        assert tmp.loc[2, '31-0.0'] == '2015-12-30'
+        assert tmp.loc[2, '34-0.0'] == 12
+        assert tmp.loc[2, '46-0.0'] == -2
+
+        tmp = pd.read_sql('select * from ukb_pheno_02', create_engine(db_engine), index_col='eid')
+        assert not tmp.empty
+        assert tmp.shape[0] == 2
+        assert tmp.loc[1, '47-0.0'].round(5) == 45.55412
+        assert tmp.loc[1, '48-0.0'] == '2011-08-14'
+        assert tmp.loc[2, '47-0.0'].round(5) == -0.55461
+        assert tmp.loc[2, '48-0.0'] == '2010-03-29'
+
+    def test_postgresql_less_columns_per_table(self):
+        # Prepare
+        csv_file = get_repository_path('pheno2sql/example01.csv')
+        db_engine = 'postgresql://test:test@localhost:5432/ukb'
+
+        p2sql = Pheno2SQL(csv_file, db_engine, n_columns_per_table=3)
+
+        # Run
+        p2sql.load_data()
+
+        # Validate
+        assert p2sql.db_type == 'postgresql'
+
+        ## Check tables exist
+        table = pd.read_sql("SELECT EXISTS (SELECT 1 FROM pg_tables WHERE schemaname = 'public' AND tablename = '{}');".format('ukb_pheno_00'), create_engine(db_engine))
+        assert table.iloc[0, 0]
+
+        table = pd.read_sql("SELECT EXISTS (SELECT 1 FROM pg_tables WHERE schemaname = 'public' AND tablename = '{}');".format('ukb_pheno_01'), create_engine(db_engine))
+        assert table.iloc[0, 0]
+
+        table = pd.read_sql("SELECT EXISTS (SELECT 1 FROM pg_tables WHERE schemaname = 'public' AND tablename = '{}');".format('ukb_pheno_02'), create_engine(db_engine))
+        assert table.iloc[0, 0]
+
+        ## Check columns are correct
+        tmp = pd.read_sql('select * from ukb_pheno_00', create_engine(db_engine))
+        expected_columns = ["eid","21-0.0","21-1.0","21-2.0"]
+        assert len(tmp.columns) == len(expected_columns)
+        assert all(x in expected_columns for x in tmp.columns)
+
+        tmp = pd.read_sql('select * from ukb_pheno_01', create_engine(db_engine))
+        expected_columns = ["eid","31-0.0","34-0.0","46-0.0"]
+        assert len(tmp.columns) == len(expected_columns)
+        assert all(x in expected_columns for x in tmp.columns)
+
+        tmp = pd.read_sql('select * from ukb_pheno_02', create_engine(db_engine))
+        expected_columns = ["eid","47-0.0","48-0.0"]
+        assert len(tmp.columns) == len(expected_columns)
+        assert all(x in expected_columns for x in tmp.columns)
+
+        ## Check data is correct
+        tmp = pd.read_sql('select * from ukb_pheno_00', create_engine(db_engine), index_col='eid')
+        assert not tmp.empty
+        assert tmp.shape[0] == 2
+        assert tmp.loc[1, '21-0.0'] == 'Option number 1'
+        assert tmp.loc[1, '21-1.0'] == 'No response'
+        assert tmp.loc[1, '21-2.0'] == 'Yes'
+        assert tmp.loc[2, '21-0.0'] == 'Option number 2'
+        assert tmp.loc[2, '21-1.0'] == ''
+        assert tmp.loc[2, '21-2.0'] == 'No'
+
+        tmp = pd.read_sql('select * from ukb_pheno_01', create_engine(db_engine), index_col='eid')
+        assert not tmp.empty
+        assert tmp.shape[0] == 2
+        assert tmp.loc[1, '31-0.0'].strftime('%Y-%m-%d') == '2012-01-05'
+        assert tmp.loc[1, '34-0.0'] == 21
+        assert tmp.loc[1, '46-0.0'] == -9
+        assert tmp.loc[2, '31-0.0'].strftime('%Y-%m-%d') == '2015-12-30'
+        assert tmp.loc[2, '34-0.0'] == 12
+        assert tmp.loc[2, '46-0.0'] == -2
+
+        tmp = pd.read_sql('select * from ukb_pheno_02', create_engine(db_engine), index_col='eid')
+        assert not tmp.empty
+        assert tmp.shape[0] == 2
+        assert tmp.loc[1, '47-0.0'].round(5) == 45.55412
+        assert tmp.loc[1, '48-0.0'].strftime('%Y-%m-%d') == '2011-08-14'
+        assert tmp.loc[2, '47-0.0'].round(5) == -0.55461
+        assert tmp.loc[2, '48-0.0'].strftime('%Y-%m-%d') == '2010-03-29'
