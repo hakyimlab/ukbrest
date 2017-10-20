98e5ed1eda8d3a1cbcda34f5b26f7a60070e8e77
Milton Pividori <miltondp@gmail.com>
2017-10-20 10:38:34 -0500

    utils/sql/sql_post_relatedness.sql: improvements.

--- utils/sql/sql_post_relatedness.sql
+++ utils/sql/sql_post_relatedness.sql
@@ -1,9 +1,15 @@
 -- this query populates a table with samples that should be discarded if 2nd and higher degree are being excluded.
--- They are the related sample with highest missing rate
+-- They are one of the two related samples with highest missing rate
 
-select * into bad_samples_relatedness_2nd_higher_and_high_missrate from (select distinct eid from (select distinct on (id1, id2) eid
-    from relatedness inner join samplesqc on (id1 = eid or id2 = eid)
-    where kinship >= 0.0883
-    order by id1, id2, sample_qc_missing_rate desc) t1) t2;
+select * into bad_related_samples_2nd_higher_and_high_missrate from (
+    select distinct eid from (
+        select distinct on (id1, id2) eid
+        from relatedness inner join samplesqc on (id1 = eid or id2 = eid)
+        where kinship >= 0.0883
+        order by id1, id2, sample_qc_missing_rate desc
+    ) t1
+) t2;
 
-alter table bad_samples_relatedness_2nd_higher_and_high_missrate add primary key (eid);
+alter table bad_related_samples_2nd_higher_and_high_missrate add primary key (eid);
+
+vacuum analyze bad_related_samples_2nd_higher_and_high_missrate;
