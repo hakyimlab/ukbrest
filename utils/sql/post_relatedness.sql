a71657b5023e25f23aac851fd23f907141355f3f
Milton Pividori <miltondp@gmail.com>
2017-10-24 16:48:10 -0500

    utils/sql/post_relatedness.sql: fix.

--- utils/sql/post_relatedness.sql
+++ utils/sql/post_relatedness.sql
@@ -5,8 +5,8 @@ select * into bad_related_samples_2nd_higher_and_high_missrate from (
     select distinct eid from (
         select distinct on (id1, id2) eid
         from relatedness inner join samplesqc on (id1 = eid or id2 = eid)
-        where kinship >= 0.0883
-        order by id1, id2, sample_qc_missing_rate desc
+        where ckinship_0_0 >= 0.0883
+        order by id1, id2, csample_qc_missing_rate_0_0 desc
     ) t1
 ) t2;
 
