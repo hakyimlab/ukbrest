e8ab4e570e35740f1ce1068278c1f8317b661ba1
Milton Pividori <miltondp@gmail.com>
2017-10-27 09:46:50 -0500

    post_samples.sql: fix.

--- utils/sql/post_samples.sql
+++ utils/sql/post_samples.sql
@@ -1,7 +1,7 @@
-drop index ix_samples_eid;
-create unique index ix_samples_eid ON samples USING btree (eid);
+drop index ix_bgen_samples_eid;
+create unique index ix_bgen_samples_eid ON bgen_samples USING btree (eid);
 
-drop index ix_samples_index;
-create unique index ix_samples_index ON samples USING btree (index);
+drop index ix_bgen_samples_index;
+create unique index ix_bgen_samples_index ON bgen_samples USING btree (index);
 
-vacuum analyze samples;
+vacuum analyze bgen_samples;
