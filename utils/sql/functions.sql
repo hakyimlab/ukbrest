2cb10d8c4223494c0ccafa152192843fb99f9b25
Milton Pividori <miltondp@gmail.com>
2018-04-19 15:31:24 -0500

    utils/sql/functions.sql: get_children_codings updated to support an array of parent ids.

--- utils/sql/functions.sql
+++ utils/sql/functions.sql
@@ -6,12 +6,12 @@ CREATE OR REPLACE FUNCTION nullifneg(integer) RETURNS integer
     RETURNS NULL ON NULL INPUT;
 
 -- Returns children codings recursively, given data field and the disease/value parent node_id
-CREATE OR REPLACE FUNCTION get_children_codings(text, integer) RETURNS setof text
+CREATE OR REPLACE FUNCTION get_children_codings(text, integer[]) RETURNS setof text
     AS '
     with recursive children_coding(coding, node_id, parent_id) as (
       select coding, node_id, parent_id
       from codings
-      where data_coding = (select distinct coding from fields where field_id = $1) and parent_id = $2
+      where data_coding = (select distinct coding from fields where field_id = $1) and parent_id = ANY($2)
     union
       select c.coding, c.node_id, c.parent_id
       from children_coding cc, codings c
@@ -31,4 +31,3 @@ CREATE OR REPLACE FUNCTION array_avg(double precision[]) RETURNS double precisio
     LANGUAGE SQL
     IMMUTABLE
     RETURNS NULL ON NULL INPUT;
-
