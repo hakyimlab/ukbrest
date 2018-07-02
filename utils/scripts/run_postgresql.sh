1e97cc9f4fa332d38edaf36a51445a1b75f41440
Milton Pividori <miltondp@gmail.com>
2018-07-02 16:27:57 -0500

    Backed out changeset: 02d8ec25882e

--- utils/scripts/run_postgresql.sh
+++ utils/scripts/run_postgresql.sh
@@ -4,14 +4,12 @@ export PGUSER="test"
 export PGPASSWORD="test"
 export DB_URI="postgresql://${PGUSER}:${PGPASSWORD}@pg:5432/ukb"
 
-PG_IMAGE="postgres:10.4"
-
-docker pull ${PG_IMAGE}
+docker pull postgres:9.6
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker run --rm --name pg --net ukb -e POSTGRES_USER=${PGUSER} \
   -e POSTGRES_PASSWORD=${PGPASSWORD} -e POSTGRES_DB=ukb -p 5432:5432 \
   -v ${DIR}/postgresql.conf:/etc/postgresql.conf \
-  ${PG_IMAGE} postgres -c config_file=/etc/postgresql.conf
+  postgres:9.6 postgres -c config_file=/etc/postgresql.conf
 
