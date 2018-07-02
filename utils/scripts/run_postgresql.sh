833e60d059ff9573373c92b52ea996ca86443ae5
Milton Pividori <miltondp@gmail.com>
2018-07-02 14:23:07 -0500

    utils/scripts/run_postgresql.sh: use PostgreSQL 10.4

--- utils/scripts/run_postgresql.sh
+++ utils/scripts/run_postgresql.sh
@@ -4,12 +4,14 @@ export PGUSER="test"
 export PGPASSWORD="test"
 export DB_URI="postgresql://${PGUSER}:${PGPASSWORD}@pg:5432/ukb"
 
-docker pull postgres:9.6
+PG_IMAGE="postgres:10.4"
+
+docker pull ${PG_IMAGE}
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker run --rm --name pg --net ukb -e POSTGRES_USER=${PGUSER} \
   -e POSTGRES_PASSWORD=${PGPASSWORD} -e POSTGRES_DB=ukb -p 5432:5432 \
   -v ${DIR}/postgresql.conf:/etc/postgresql.conf \
-  postgres:9.6 postgres -c config_file=/etc/postgresql.conf
+  ${PG_IMAGE} postgres -c config_file=/etc/postgresql.conf
 
