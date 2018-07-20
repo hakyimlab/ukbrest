f4dc406df28e61fafa9cf8ae4bc4a7e075fdd2b2
Milton Pividori <miltondp@gmail.com>
2018-07-20 13:18:06 -0500

    run_postgresql.sh: minor improvements and docker network fix.

--- utils/scripts/run_postgresql.sh
+++ utils/scripts/run_postgresql.sh
@@ -3,13 +3,15 @@
 export PGUSER="test"
 export PGPASSWORD="test"
 export DB_URI="postgresql://${PGUSER}:${PGPASSWORD}@pg:5432/ukb"
+export DOCKER_IMAGE="postgres:9.6"
 
-docker pull postgres:9.6
+docker network create ukb
+docker pull ${DOCKER_IMAGE}
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker run --rm --name pg --net ukb -e POSTGRES_USER=${PGUSER} \
   -e POSTGRES_PASSWORD=${PGPASSWORD} -e POSTGRES_DB=ukb -p 5432:5432 \
   -v ${DIR}/postgresql.conf:/etc/postgresql.conf \
-  postgres:9.6 postgres -c config_file=/etc/postgresql.conf
+  ${DOCKER_IMAGE} postgres -c config_file=/etc/postgresql.conf
 
