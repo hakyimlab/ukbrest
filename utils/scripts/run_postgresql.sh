f61f2fac30845d10fb6073c2d5f096bd575478ca
Milton Pividori <miltondp@gmail.com>
2018-11-24 19:12:22 -0600

    utils/sccripts: updated PostgreSQL used in tests to version 11.

--- utils/scripts/run_postgresql.sh
+++ utils/scripts/run_postgresql.sh
@@ -3,7 +3,7 @@
 export PGUSER="test"
 export PGPASSWORD="test"
 export DB_URI="postgresql://${PGUSER}:${PGPASSWORD}@pg:5432/ukb"
-export DOCKER_IMAGE="postgres:9.6"
+export DOCKER_IMAGE="postgres:11"
 
 docker network create ukb
 docker pull ${DOCKER_IMAGE}
@@ -13,5 +13,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker run --rm --name pg --net ukb -e POSTGRES_USER=${PGUSER} \
   -e POSTGRES_PASSWORD=${PGPASSWORD} -e POSTGRES_DB=ukb -p 5432:5432 \
   -v ${DIR}/postgresql.conf:/etc/postgresql.conf \
-  ${DOCKER_IMAGE} postgres -c config_file=/etc/postgresql.conf
+  ${DOCKER_IMAGE} postgres -c 'config_file=/etc/postgresql.conf'
 
