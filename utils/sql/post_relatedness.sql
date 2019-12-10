5cfa2fbfa2bfcb3fcd0f67766e8e18b87eec4413
Milton Pividori <miltondp@gmail.com>
2019-12-10 13:50:15 -0600

    utils/sql/post_relatedness.sql: eids sorting is consistent when missing rate are the same in pairs.

--- utils/sql/post_relatedness.sql
+++ utils/sql/post_relatedness.sql
@@ -1,12 +1,13 @@
 -- this query populates a table with samples that should be discarded if 2nd and higher degree are being excluded.
 -- They are one of the two related samples with highest missing rate
+-- If both samples in a pair have the same missing rate, then the one with the least eid is selected
 
 select * into bad_related_samples_2nd_higher_and_high_missrate from (
     select distinct eid from (
         select distinct on (id1, id2) eid
         from relatedness inner join samplesqc on (id1 = eid or id2 = eid)
         where ckinship_0_0 >= 0.0883
-        order by id1, id2, csample_qc_missing_rate_0_0 desc
+        order by id1, id2, csample_qc_missing_rate_0_0 desc, eid asc
     ) t1
 ) t2;
 
