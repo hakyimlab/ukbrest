b4279a171df097265f7178cd8529bfde29b18f03
meliao <owenjmelia@gmail.com>
2020-05-04 12:59:48 +0000

    Moved some files to use via command line

---
+++ load_data.py
@@ -0,0 +1,109 @@
+import os
+import argparse
+
+from ukbrest.common.pheno2sql import Pheno2SQL
+from ukbrest.common.postloader import Postloader
+from ukbrest import config
+from ukbrest.common.utils.misc import update_parameters_from_args, parameter_empty
+
+from ukbrest.resources.error_handling import handle_errors
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--load-sql', action='store_true')
+parser.add_argument('--load-withdrawals', action='store_true')
+parser.add_argument('--load-codings', action='store_true')
+parser.add_argument('--load-samples-data', action='store_true')
+parser.add_argument('--identifier-columns', type=str, nargs='+', help='Format file1.txt:column1 file2.txt:column2 ...')
+parser.add_argument('--skip-columns', type=str, nargs='+', help='Format file1.txt:column1 file2.txt:column2 ...')
+parser.add_argument('--separators', type=str, nargs='+', help='Format file1.txt:column1 file2.txt:column2 ...')
+
+
+@handle_errors
+def load_withdrawals(args):
+    pl = Postloader(**config.get_postloader_parameters())
+    pl.load_withdrawals(**config.get_postloader_withdrawals_parameters())
+
+
+@handle_errors
+def load_codings(args):
+    pl = Postloader(**config.get_postloader_parameters())
+    pl.load_codings(**config.get_postloader_codings_parameters())
+
+
+@handle_errors
+def load_samples_data(args):
+    pl = Postloader(**config.get_postloader_parameters())
+
+    load_samples_parameters = config.get_postloader_samples_data_parameters()
+
+    if args.identifier_columns is not None:
+        load_samples_parameters.update(
+            {'identifier_columns': {p.split(':')[0]: p.split(':')[1].split(',') for p in args.identifier_columns}})
+
+    if args.skip_columns is not None:
+        load_samples_parameters.update(
+            {'skip_columns': {p.split(':')[0]: p.split(':')[1].split(',') for p in args.skip_columns}})
+
+    if args.separators is not None:
+        load_samples_parameters.update({'separators': {p.split(':')[0]: p.split(':')[1] for p in args.separators}})
+
+    pl.load_samples_data(**load_samples_parameters)
+
+
+#@handle_errors
+def load_data(args):
+    pheno2sql_parameters = config.get_pheno2sql_parameters()
+    pheno2sql_parameters = update_parameters_from_args(pheno2sql_parameters, args)
+
+    # FIXME: parameter names hard coded here
+    if parameter_empty(pheno2sql_parameters, 'ukb_csvs'):
+        if args.pheno_dir is None:
+            parser.error('--pheno-dir missing')
+
+        pheno2sql_parameters['ukb_csvs'] = sorted([
+            os.path.join(args.pheno_dir, f)
+            for f in os.listdir(args.pheno_dir)
+            if f.lower().endswith('.csv')
+        ])
+
+    if parameter_empty(pheno2sql_parameters, 'db_uri'):
+        parser.error('--db-uri missing')
+
+    p2sql = Pheno2SQL(**pheno2sql_parameters)
+
+    load_parameters = config.get_pheno2sql_load_parameters()
+
+    p2sql.load_data(**load_parameters)
+
+
+#@handle_errors
+def load_sql():
+    pheno2sql_parameters = config.get_pheno2sql_parameters()
+    pheno2sql_parameters = update_parameters_from_args(pheno2sql_parameters, args)
+
+    if parameter_empty(pheno2sql_parameters, 'db_uri'):
+        parser.error('--db-uri missing')
+
+    p2sql = Pheno2SQL(**pheno2sql_parameters)
+
+    p2sql.load_sql('/opt/utils/sql/functions.sql')
+
+
+if __name__ == '__main__':
+    parser = config.get_argparse_arguments(parser)
+    args, unknown_args = parser.parse_known_args()
+
+    if args.load_codings:
+        load_codings(args)
+
+    elif args.load_withdrawals:
+        load_withdrawals(args)
+
+    elif args.load_samples_data:
+        load_samples_data(args)
+
+    elif args.load_sql:
+        load_sql()
+
+    else:
+        load_data(args)
---
+++ start.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+
+from os import listdir, execvp
+from os import environ
+from os.path import isdir, join, basename
+import argparse
+import re
+
+from ukbrest.config import logger, GENOTYPE_PATH_ENV, PHENOTYPE_PATH, PHENOTYPE_CSV_ENV, DB_URI_ENV, CODINGS_PATH, \
+    SAMPLES_DATA_PATH, WITHDRAWALS_PATH
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--load', action='store_true', help='Specifies whether data should be loaded into the DB.')
+parser.add_argument('--load-sql', action='store_true', help='Loads some useful SQL functions into the database.')
+parser.add_argument('--load-codings', action='store_true', help='Loads a set of codings files (coding_NUM.tsv).')
+parser.add_argument('--load-withdrawals', action='store_true', help='Loads a list of participants who has withdrawn consent (*.csv files).')
+parser.add_argument('--load-samples-data', action='store_true', help='Loads a set of files containing information about samples.')
+
+args, unknown_args = parser.parse_known_args()
+
+
+def _setup_genotype_path():
+    genotype_path = environ.get(GENOTYPE_PATH_ENV, None)
+
+    if not isdir(genotype_path):
+        logger.warning('The genotype directory does not exist. You have to mount it using '
+                       'the option "-v hostDir:{}" of "docker run"'.format(genotype_path))
+        return
+
+    bgen_files = [f for f in listdir(genotype_path) if f.lower().endswith('.bgen')]
+    if len(bgen_files) == 0:
+        logger.warning('No .bgen files were found in the genotype directory')
+
+    bgi_files = [f for f in listdir(genotype_path) if f.lower().endswith('.bgi')]
+    if len(bgi_files) == 0:
+        logger.warning('No .bgi files were found in the genotype directory')
+
+
+def _setup_phenotype_path():
+    phenotype_path = environ.get(PHENOTYPE_PATH, None)
+
+    if not isdir(phenotype_path):
+        parser.error('The phenotype directory does not exist. You have to mount it using '
+                     'the option "-v hostDir:{}" of "docker run"'.format(phenotype_path))
+
+    filename_number_pattern = re.compile('(?P<dataset_id>\d+)')
+
+    def sort_datasets(f):
+        """Returns the first number found in the filename as a float. If none is found, then return a minimum number."""
+        filename = basename(f)
+
+        m = re.search(filename_number_pattern, filename)
+        if m is not None:
+            return float(m.group('dataset_id'))
+
+        return float('-inf')
+
+    # by default, sort .csv files in reverse order taking the first number found in their names.
+    # So for instance, these files: ukb00.csv, ukb01.csv and ukb50.csv would be loaded in
+    # this order: ukb50.csv, ukb01.csv and ukb00.csv
+    # the number in the file is interpreted as the dataset id, and greater means newer.
+    phenotype_csv_file = sorted(
+        [f for f in listdir(phenotype_path) if f.lower().endswith('.csv')],
+        key=sort_datasets,
+        reverse=True
+    )
+
+    # check whether there is at least one and only one csv file
+    if len(phenotype_csv_file) == 0:
+        parser.error('No .csv files were found in the phenotype directory')
+
+    environ[PHENOTYPE_CSV_ENV] = ';'.join([join(phenotype_path, csv_file) for csv_file in phenotype_csv_file])
+
+
+def _setup_codings():
+    phenotype_path = environ.get(PHENOTYPE_PATH, None)
+    coding_path = environ.get(CODINGS_PATH, None)
+
+    if coding_path is None:
+        environ[CODINGS_PATH] = 'codings'
+        coding_path = 'codings'
+
+    coding_path = join(phenotype_path, coding_path)
+
+    if not isdir(coding_path):
+        parser.error('The codings directory does not exist: {}'.format(coding_path))
+
+
+def _setup_withdrawals():
+    withdrawals_path = environ.get(WITHDRAWALS_PATH, None)
+
+    if withdrawals_path is None:
+        parser.error('The withdrawals directory was not specified')
+
+    if not isdir(withdrawals_path):
+        parser.error('The withdrawals directory does not exist: {}'.format(withdrawals_path))
+
+
+def _setup_samples_data():
+    phenotype_path = environ.get(PHENOTYPE_PATH, None)
+    samples_data_path = environ.get(SAMPLES_DATA_PATH, None)
+
+    if samples_data_path is None:
+        environ[SAMPLES_DATA_PATH] = 'samples_data'
+        samples_data_path = 'samples_data'
+
+    samples_data_path = join(phenotype_path, samples_data_path)
+
+    if not isdir(samples_data_path):
+        parser.error('The samples data directory does not exist: {}'.format(samples_data_path))
+
+
+def _setup_db_uri():
+    db_uri = environ.get(DB_URI_ENV, None)
+
+    if db_uri is None:
+        parser.error('No DB URI was specified. You have to set it using the environment variable UKBREST_DB_URI. For '
+                     'example, for PostgreSQL, the format is: postgresql://user:pass@host:port/dbname')
+
+
+if __name__ == '__main__':
+    if args.load:
+        _setup_phenotype_path()
+        _setup_db_uri()
+
+        commands = ('python', ['python', '/opt/ukbrest/load_data.py'])
+
+    elif args.load_sql:
+        _setup_db_uri()
+
+        commands = ('python', ['python', '/opt/ukbrest/load_data.py', '--load-sql'])
+
+    elif args.load_codings:
+        _setup_codings()
+        _setup_db_uri()
+
+        commands = ('python', ['python', '/opt/ukbrest/load_data.py', '--load-codings'])
+
+    elif args.load_withdrawals:
+        _setup_withdrawals()
+        _setup_db_uri()
+
+        commands = ('python', ['python', '/opt/ukbrest/load_data.py', '--load-withdrawals'])
+
+    elif args.load_samples_data:
+        _setup_samples_data()
+        _setup_db_uri()
+
+        commands = ('python', ['python', '/opt/ukbrest/load_data.py', '--load-samples-data'] + unknown_args)
+
+    else:
+        _setup_genotype_path()
+        _setup_db_uri()
+        # TODO: check if data was loaded into PostgreSQL
+
+        commands = ('gunicorn', ['gunicorn', 'ukbrest.wsgi:app'])
+
+    execvp(*commands)
